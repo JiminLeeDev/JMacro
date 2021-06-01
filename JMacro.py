ebfc57eabc90629790680a445757b6678b9edd71
JiminLeeDev <JiminLeeDev0220@gmail.com>
2021-06-02 03:08:47 +0900

    Configure file save button was implemented. if button is clicked, configure file is saved.

--- JMacro.py
+++ JMacro.py
@@ -4,55 +4,66 @@ import os
 import pyautogui
 
 from PyQt5.QtCore import QTimer
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QPushButton, QWidget
 
-configures = {
-    'ClickInterval': 1000,
-    'ImageAddress':[
-        'Cap 2021-06-02 02-15-00-602.png'
-    ]
-}
+class ConfigureManager():
+    def __init__(self):
+        self.configures = {}
+        
+    def SaveConfigureFile(self):
+        f = open("configures.json", "w")
+        json.dump(self.configures, f, indent='\t')
+        f.close()
 
-def CreateConfigureFile(jsonData):
-    f = open("configures.json", "w")
-    json.dump(jsonData, f, indent='\t')
-    f.close()
-    
-def LoadConfigureFile():
-    f = open("configures.json", "r")
-    jsonData = f.read().replace(' ', '').replace('\n','').replace('\t','')
-    f.close()
-    configures = json.loads(jsonData)
+    def CreateConfigureFile(self):
+        self.configures = {    
+            'ClickInterval': 1000,
+            'ImageAddress':[
+                'Cap 2021-06-02 02-15-00-602.png'
+            ]
+        }
 
-CreateConfigureFile(configures)
-def Macro():
-    for name in configures['ImageAddress']:
-        name = "resource/" + name
+    def LoadConfigureFile(self):
+        f = open("configures.json", "r")
+        jsonData = f.read().replace(' ', '').replace('\n','').replace('\t','')
+        f.close()
+        self.configures = json.loads(jsonData)
 
-        if os.path.isfile(name):
-            btnLocation = pyautogui.locateOnScreen(name)
-
-            if btnLocation != None:
-                pyautogui.doubleClick(btnLocation)
 
 
 class MyApp(QWidget):
     def __init__(self):
         super().__init__()
+        self.configureManager = ConfigureManager()
+        self.configureManager.CreateConfigureFile()
         self.initUI()
         self.setTimer()
         
     def initUI(self):
-        self.setWindowTitle("JMacro")
-        self.move(300, 300)
-        self.resize(400, 200)
+        self.configureSaveBtn = QPushButton(self)
+        self.configureSaveBtn.setText('save configure')
+        self.configureSaveBtn.setGeometry(10,10,100,30)   
+        self.configureSaveBtn.clicked.connect(self.configureManager.SaveConfigureFile)   
+
+        self.setWindowTitle('JMacro')
+        self.setGeometry(300,300,500,500) 
         self.show()
 
     def setTimer(self):
         self.timer = QTimer()
-        self.timer.setInterval(configures['ClickInterval'])
-        self.timer.timeout.connect(Macro)
+        self.timer.setInterval(self.configureManager.configures['ClickInterval'])
+        self.timer.timeout.connect(self.Macro)
         self.timer.start()
+
+    def Macro(self):
+        for name in self.configureManager.configures['ImageAddress']:
+            name = "resource/" + name
+
+            if os.path.isfile(name):
+                btnLocation = pyautogui.locateOnScreen(name)
+
+                if btnLocation != None:
+                    pyautogui.doubleClick(btnLocation)
 
 
 if __name__ == "__main__":
