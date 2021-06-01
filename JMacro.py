4916d9d2c816dd4397be0c23b9ea949298db593c
JiminLeeDev <JiminLeeDev0220@gmail.com>
2021-06-02 03:26:25 +0900

    macro on/off button added.

--- JMacro.py
+++ JMacro.py
@@ -1,62 +1,77 @@
-import json 
+import json
 import sys
-import os   
+import os
 import pyautogui
 
 from PyQt5.QtCore import QTimer
 from PyQt5.QtWidgets import QApplication, QPushButton, QWidget
 
-class ConfigureManager():
+
+class ConfigureManager:
     def __init__(self):
         self.configures = {}
-        
+
     def SaveConfigureFile(self):
         f = open("configures.json", "w")
-        json.dump(self.configures, f, indent='\t')
+        json.dump(self.configures, f, indent="\t")
         f.close()
 
     def CreateConfigureFile(self):
-        self.configures = {    
-            'ClickInterval': 1000,
-            'ImageAddress':[
-                'Cap 2021-06-02 02-15-00-602.png'
-            ]
+        self.configures = {
+            "ClickInterval": 1000,
+            "ImageAddress": ["Cap 2021-06-02 02-15-00-602.png"],
         }
 
     def LoadConfigureFile(self):
         f = open("configures.json", "r")
-        jsonData = f.read().replace(' ', '').replace('\n','').replace('\t','')
+        jsonData = f.read().replace(" ", "").replace("\n", "").replace("\t", "")
         f.close()
         self.configures = json.loads(jsonData)
-
 
 
 class MyApp(QWidget):
     def __init__(self):
         super().__init__()
+
         self.configureManager = ConfigureManager()
         self.configureManager.CreateConfigureFile()
+
         self.initUI()
         self.setTimer()
-        
+
     def initUI(self):
         self.configureSaveBtn = QPushButton(self)
-        self.configureSaveBtn.setText('save configure')
-        self.configureSaveBtn.setGeometry(10,10,100,30)   
-        self.configureSaveBtn.clicked.connect(self.configureManager.SaveConfigureFile)   
+        self.configureSaveBtn.setText("save configure")
+        self.configureSaveBtn.setGeometry(10, 10, 100, 30)
+        self.configureSaveBtn.clicked.connect(self.configureManager.SaveConfigureFile)
 
-        self.setWindowTitle('JMacro')
-        self.setGeometry(300,300,500,500) 
+        self.macroBtn = QPushButton(self)
+        self.macroBtn.setText("macro on")
+        self.macroBtn.setGeometry(10, 50, 100, 30)
+        self.macroBtn.clicked.connect(self.macroBtnClicked)
+
+        self.setWindowTitle("JMacro")
+        self.setGeometry(300, 300, 500, 500)
         self.show()
+
+    def macroBtnClicked(self):
+        btn = self.sender()
+
+        if btn.text() == "macro off":
+            btn.setText("macro on")
+            self.timer.stop()
+        else:
+            btn.setText("macro off")
+            self.timer.start()
 
     def setTimer(self):
         self.timer = QTimer()
-        self.timer.setInterval(self.configureManager.configures['ClickInterval'])
+        self.timer.setInterval(self.configureManager.configures["ClickInterval"])
         self.timer.timeout.connect(self.Macro)
         self.timer.start()
 
     def Macro(self):
-        for name in self.configureManager.configures['ImageAddress']:
+        for name in self.configureManager.configures["ImageAddress"]:
             name = "resource/" + name
 
             if os.path.isfile(name):
